8490e222a31f48e13914fef32b307aed926daa78
Thomas <thomas.gitcommit@outlook.com>
2015-07-31 18:04:34 +1000

    Added structs for a Health/Damage system

---
+++ TGameLibrary/AnimatedSpriteStucts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TGameLibrary
+{
+    /// <summary>
+    /// An animated sprite.
+    /// </summary>
+    public partial class AnimatedSprite
+    {
+        public struct Health
+        {
+            public static int Max = 100;
+            public static int Current = Max;
+        }
+
+        public struct Armour
+        {
+            public static float Generic = 0;
+            public static float Stabbing = 0;
+            public static float Piercing = 0;
+            public static float Crushing = 0;
+        }
+
+        public struct Damage
+        {
+            public static float Generic = 0;
+            public static float Stabbing = 0;
+            public static float Piercing = 0;
+            public static float Crushing = 0;
+        }
+
+        public struct Status
+        {
+            public static bool Invunerable = true;
+        }
+    }
+}
