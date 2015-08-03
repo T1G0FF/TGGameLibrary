ed4266fb071630ea062abff0d1f526c1eb8ea690
Thomas <thomas.gitcommit@outlook.com>
2015-08-03 22:57:44 +1000

    Added GameState class.

---
+++ TGameLibrary/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TGameLibrary
+{
+    class GameState
+    {
+        public GameStates Current = GameStates.Menu;
+
+        public enum GameStates { Menu, Loading, Playing, Paused };
+    }
+}
